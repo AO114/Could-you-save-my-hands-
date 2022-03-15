f5411c1a219ac17e25103bffb9877bcc0f66add1
Faith <101516609+AO114@users.noreply.github.com>
2022-03-15 23:39:09 +0800

    Add files via upload

---
+++ ResourceDictionary.py
@@ -0,0 +1,90 @@
+start = "./img/start.png"
+start1 = "./img/start1.png"
+finish = "./img/finish.png"
+
+
+#指挥分队按钮，点两次，第二次是确认
+zhihuifendui = (335,820)
+
+#取长补短选项，点两次，第二次是确认
+quchangbuduan = (1390, 644)
+
+jinwei = (734, 828)
+shan = (842, 838)
+
+fuzhu = (1154, 822)
+xinlan = (894, 220)
+
+yiliao = (1600, 818)
+furong = (896, 416)
+
+#选完人以后跳过
+skip = (2173,85)
+#选完干员以后确认
+querenganyuan = (2092,1024)
+
+speed_1x = "./img/1x_speed.png"
+bottom = (1169,920)
+
+kaishixingdong = "./img/kaishixingdong.png"
+success_pass = "./img/success_pass.png"
+nazou = "./img/nazou.png"
+exit = "./img/exit.png"
+exit_confirm = "./img/exit_confirm.png"
+signal_lost = "./img/signal_lost.png"
+fight_lipaoxiaodui = "./img/fight_lipaoxiaodui.png"
+fight_yuchongweiban = "./img/fight_yuchongweiban.png"
+fight_xunshouxiaowu = "./img/fight_xunshouxiaowu.png"
+fight_yiwai = "./img/fight_yiwai.png"
+enter = "./img/enter.png"
+
+buqieryu = "./img/buqieryu.png"
+enter_buqieryu = "./img/enter_buqieryu.png"
+choose_confirm = "./img/choose_confirm.png"
+taopao = "./img/taopao.png"
+xiwang = "./img/xiwang.png"
+shengming = "./img/shengming.png"
+yuanshiding = "./img/yuanshiding.png"
+
+guiyixingshang = "./img/guiyixingshang.png"
+enter_guiyixingshang = "./img/enter_guiyixingshang.png"
+touzi_enter = "./img/touzi_enter.png"
+touzirukou = "./img/touzirukou.png"
+touzi_confirm = "./img/touzi_confirm.png"
+suanle = "./img/suanle.png"
+suanle2 = "./img/suanle2.png"
+exit_shop = "./img/exit_shop.png"
+
+mujianyuxing = "./img/mujianyuxing.png"
+
+exit_all = "./img/exit_all.png"
+giveup = "./img/giveup.png"
+giveup_confirm = "./img/giveup_confirm.png"
+
+rg_start = "./img/rg_start.png"
+enter_game = "./img/enter_game.png"
+
+right_slide_down = ((1846, 844), (1846, 232))
+bottom_slide_left = ((1760, 860), (350, 860))
+quchangbuduan = (1380, 856)
+
+yuchongweiban_shan = (1092, 654)
+yuchongweiban_furong = (1476, 622)
+yuchongweiban_xinlan = (1210, 528)
+yiwai_shan = (1528, 358)
+yiwai_furong = (1652, 234)
+yiwai_xinlan = (1404, 348)
+lipaoxiaodui_shan = (962, 504)
+lipaoxiaodui_furong = (808, 730)
+lipaoxiaodui_xinlan = (970, 378)
+xunshouxiaowu_shan = (876, 726)
+xunshouxiaowu_furong = (874, 464)
+xunshouxiaowu_xinlan = (1012, 456)
+
+xilan_fightingicon = "./img/xilan_fightingicon.png"
+shan_fightingicon = "./img/shan_fightingicon.png"
+furong_fightingicon = "./img/furong_fightingicon.png"
+
+fight_icon_xinlan = "./img/xilan_fightingicon.png"
+fight_icon_shan = "./img/shan_fightingicon.png"
+fight_icon_furong = "./img/furong_fightingicon.png"
---
+++ test.py
@@ -0,0 +1,7 @@
+fight_icon_xinlan = "./img/xilan_fightingicon.png"
+fight_icon_shan = "./img/shan_fightingicon.png"
+fight_icon_furong = "./img/furong_fightingicon.png"
+
+xilan_fightingicon = "./img/xilan_fightingicon.png"
+shan_fightingicon = "./img/shan_fightingicon.png"
+furong_fightingicon = "./img/furong_fightingicon.png"
